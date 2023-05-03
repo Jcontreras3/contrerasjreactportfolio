cda7fb30ac1485a6f3b810ee5aa32168dbc83b62
Jovann Contreras <jcontreras@codestack.co>
2023-05-03 12:08:14 -0700

    fixed some items added package fro icons

--- src/Components/Portfoilio/PortfolioComponent.js
+++ src/Components/Portfoilio/PortfolioComponent.js
@@ -16,7 +16,8 @@ import allforone from "../../Assets/allforone.png";
 import HamsterHotel from "../../Assets/HamsterHotel.png";
 import AdviceGenerator from "../../Assets/advicegen.png";
 import BudgetApp from "../../Assets/Budgetapp.png";
-import PokemonApp from"../../Assets/PokemonGen.png";
+import PokemonApp from "../../Assets/PokemonGen.png";
+import { Icon } from "@iconify/react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Link } from "@mui/material";
@@ -65,95 +66,123 @@ export default function PortfolioComponent() {
             <h2 className="introTxt">Frontend Developer</h2>
             <p className="introTxt">
               Hello welcome to my portfolio webpage, my name is Jovann Contreras
-              I am Junior Software Engineer who enjoys working on the frontend{" "}
+              I am Junior Software Engineer who enjoys working on the frontend
             </p>
           </div>
         </Row>
-        
-          <div className="projectsBx">
-            <h2 className="projectsTitle">Projects and more to come!</h2>
-            <div className="allGroup">
-              <p className="allTitle">All For One</p>
-              <div className="languagesUsed">
-                <HtmlIcon /> <JavascriptIcon /> <CssIcon />
-              </div>
-              <Link
-                className=""
-                href="https://jcontreras3.github.io/ContrerasJAllForOneSprintDay2/"
-              >
-                <img className="allforonestyle" src={allforone} />
-              </Link>
+
+        <Row className="projectsBx">
+          <h2 className="projectsTitle">Projects</h2>
+          <div className="allGroup">
+            <p className="allTitle">All For One</p>
+            <div className="languagesUsed">
+              <HtmlIcon /> <JavascriptIcon /> <CssIcon />
             </div>
-            <div className="hamsterGroup">
-              <p className="hamsterTitle">Hamster Hotel</p>
-              <div className="languagesUsed">
-                <HtmlIcon /> <CssIcon />
-              </div>
-              <Link
-                className=""
-                href="https://jcontreras3.github.io/ContrerasJHamsterHotel/"
-              >
-                <img className="hamsterHotelstyle" src={HamsterHotel} />
-              </Link>
-            </div>
-            <div className="adviceGroup">
-              <p className="adviceTitle">Advice Generator</p>
-              <div className="languagesUsed">
-                <p className="reactTxt">React <JavascriptIcon /> <CssIcon /></p>
-               
-              </div>
-              <Link
-                className=""
-                href="https://jovannadvicegenerator.azurewebsites.net"
-              >
-                <img className="adviceGenstyle" src={AdviceGenerator} />
-              </Link>
-            </div>
-            <div className="budgetGroup">
-              <p className="budgetTitle">Budgetting App</p>
-              <div className="languagesUsed">
-                 <JavascriptIcon /> <CssIcon /> <HtmlIcon/>
-               
-              </div>
-              <Link
-                className=""
-                href="https://jcontreras3.github.io/ContrerasJBudgetApp/"
-              >
-                <img className="budgetGenstyle" src={BudgetApp} />
-              </Link>
-            </div>
-            <div className="pokeGroup">
-              <p className="pokeTitle">Pokemon Generator</p>
-              <div className="languagesUsed">
-              <p className="reactTxt">React <JavascriptIcon /> <CssIcon /></p>
-               
-              </div>
-              <Link
-                className=""
-                href="http://contrerasjovannpokedex.azurewebsites.net"
-              >
-                <img className="pokeGenstyle" src={PokemonApp} />
-              </Link>
-            </div>
+            <Link
+              className=""
+              href="https://jcontreras3.github.io/ContrerasJAllForOneSprintDay2/"
+            >
+              <img className="allforonestyle" src={allforone} />
+            </Link>
           </div>
-        
+          <div className="hamsterGroup">
+            <p className="hamsterTitle">Hamster Hotel</p>
+            <div className="languagesUsed">
+              <HtmlIcon /> <CssIcon />
+            </div>
+            <Link
+              className=""
+              href="https://jcontreras3.github.io/ContrerasJHamsterHotel/"
+            >
+              <img className="hamsterHotelstyle" src={HamsterHotel} />
+            </Link>
+          </div>
+          <div className="adviceGroup">
+            <p className="adviceTitle">Advice Generator</p>
+            <div className="languagesUsed">
+              <Icon icon="mdi:react" /> <JavascriptIcon /> <CssIcon />
+            </div>
+            <Link
+              className=""
+              href="https://jovannadvicegenerator.azurewebsites.net"
+            >
+              <img className="adviceGenstyle" src={AdviceGenerator} />
+            </Link>
+          </div>
+          <div className="budgetGroup">
+            <p className="budgetTitle">Budgetting App</p>
+            <div className="languagesUsed">
+              <JavascriptIcon /> <CssIcon /> <HtmlIcon />
+            </div>
+            <Link
+              className=""
+              href="https://jcontreras3.github.io/ContrerasJBudgetApp/"
+            >
+              <img className="budgetGenstyle" src={BudgetApp} />
+            </Link>
+          </div>
+          <div className="pokeGroup">
+            <p className="pokeTitle">Pokemon Generator</p>
+            <div className="languagesUsed">
+              <Icon icon="mdi:react" /> <JavascriptIcon /> <CssIcon />
+            </div>
+            <Link
+              className=""
+              href="http://contrerasjovannpokedex.azurewebsites.net"
+            >
+              <img className="pokeGenstyle" src={PokemonApp} />
+            </Link>
+          </div>
+        </Row>
+
         <Row className="lanRow">
-          <div className="">
-            <h2 className="lanTxt">Languages/Frameworks/Productivity Tools</h2>
-            <p className="lanTxt2">
-              These Are some of the languages I have learnt at CodeStackAcademy!
-              A one year program that I attended to obtain these skills.
-            </p>
-          </div>
-          <div>
+          <div className="languagesBx">
             <h3 className="lanTitle">Languages</h3>
-            <p>dfdf</p>
+            <div className="iconGroup">
+              <Icon icon="material-symbols:html" width="75" height="75" />
+              <Icon icon="material-symbols:javascript" width="75" height="75" />
+              <Icon
+                className=""
+                width="75"
+                height="75"
+                icon="teenyicons:c-sharp-solid"
+              />
+              <Icon
+                className="sqlStyle"
+                width="75"
+                height="75"
+                icon="tabler:sql"
+              />
+              <Icon
+                className="typeStyle"
+                width="75"
+                height="75"
+                icon="mdi:language-typescript"
+              />
+            </div>
           </div>
-          <div>
-            <p>dfdf</p>
+
+          <div className="languagesBxTwo">
+            <h3 className="lanTitle">Frameworks/Libaries</h3>
+            <div className="iconGroup">
+              <Icon icon="mdi:react" width="40" height="40" />
+              <Icon icon="tabler:brand-react-native" width="40" height="40" />
+              <Icon icon="skill-icons:bootstrap" width="40" height="40" />
+              <Icon icon="devicon:tailwindcss" width="40" height="40" />
+              <Icon icon="ph:file-css-bold" width="40" height="40" />
+            </div>
           </div>
-          <div>
-            <p>dfdf</p>
+          <div className="languagesBxThree">
+            <h3 className="lanTitle">Productivity Tools</h3>
+            <div className="iconGroup">
+              <Icon icon="logos:notion-icon" width="40" height="40" />
+              <Icon icon="ph:figma-logo-duotone" width="40" height="40" />
+              <Icon icon="simple-icons:adobexd" width="40" height="40" />
+              <Icon icon="devicon:jira-wordmark" width="40" height="40" />
+              <Icon icon="devicon:azure" width="40" height="40" />
+              <Icon icon="logos:postman-icon" width="40" height="40" />
+              <Icon icon="vscode-icons:file-type-vscode" width="40" height="40" />
+            </div>
           </div>
         </Row>
       </Container>
